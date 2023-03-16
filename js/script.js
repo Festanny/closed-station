01e65c7d1c0fc0d31fb524d342520d57f718b6e0
Festanny <60522731+Festanny@users.noreply.github.com>
2023-03-16 20:52:55 +0400

    add preloader and fix

--- js/script.js
+++ js/script.js
@@ -14,6 +14,13 @@ $(window).resize(function () {
 });
 
 window.onload = function() {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
+    }, 500);
+    document.body.style.overflow = 'auto';
+
     transformSlide()
  };
 
